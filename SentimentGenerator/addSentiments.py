b153cd34e3844cf7a5b2f0978482e2e603d96b11
KakitLee <leejacky20130710@gmail.com>
2015-04-29 23:50:32 +1000

    few changes helping the classifier to be more accurate

--- SentimentGenerator/addSentiments.py
+++ SentimentGenerator/addSentiments.py
@@ -2,6 +2,10 @@ import pickle
 import couchdb
 import re
 
+word_feature_filePath = "./word_features.txt"
+classifier_filePath = "./classifier_Utral"
+pattern = re.compile('(([A-z]{2,}(\'t)?))|([:;]o?\){1,3})|(>?[:;]o?\()')
+
 
 #Some helper function
 ################################################################################
@@ -24,37 +28,51 @@ def split_into_words(tweet,pattern):
     return words_filtered
 ################################################################################
 
-dirOfWord_feature = 'Desktop/Cloud Assignment/Map Reduce Analysis/word_features'
-dirOfClassifier = 'Desktop/Cloud Assignment/my_classifier.pickle'
+classifier_list = []
 
-# Get the classifier 
-f = open(dirOfClassifier)
-classifier = pickle.load(f)
-f.close()
+word_features = [line.strip() for line in open(word_feature_filePath, 'r')]
 
-# Get the word_feature(all the possible word in the classifer) 
-thefile = open(dirOfWord_feature, 'r')
-word_features = [line.strip() for line in thefile]
-thefile.close()
+
+# Read all the word_feature and classifier into the lists
+for i in range(1,4):    
+    new_classifier_filePath = classifier_filePath + str(i) + ".pickle"
+    f = open(new_classifier_filePath)
+    new_classifier = pickle.load(f)
+    classifier_list.append(new_classifier)
+    f.close()
 
 # Get the couchdb database
 couch = couchdb.Server()
 db = couch['new_york']
 
-# Set the pattern of the regular expression in order to split the sentence
-pattern = re.compile('([A-z]{2,})|([:;]o?\){1,3})|(>?[:;]o?\()')
-
+answer = {1:'Positive',-1:'Negative'}
 
 # Update document in new_york( Adding sentiments)
 for fid in db:
     doc = db[fid]
-    if 'sentiments' in doc:
+    if 'sentiment' in doc:
         continue
     tweet = doc['text']
     tweet = split_into_words(tweet,pattern)
-    doc['sentiments'] = classifier.classify(extract_features(tweet))
+    count = 0
     print tweet
-    print 'sentiment = ', doc['sentiments']
+    for i in range(3):
+        individual_result = classifier_list[i].classify(extract_features(tweet,word_features))
+        if individual_result == '0':
+            individual_result = -1
+        else:
+            individual_result = 1             
+        # some exception case that is hard to trained to classify too strong
+        if 'not' in tweet:
+            if 'good' in tweet or 'happy' in tweet or 'success' in tweet or 'funny' in tweet:
+                individual_result = -1 
+        count = count + individual_result
+        print "classifier " +  str(i) + " beliver " + "sentiment = ", answer[individual_result]
+    if count > 0:
+        doc['sentiment'] = 'Positive'
+    else:
+        doc['sentiment'] = 'Negative'
+    print 'sentiment = ', doc['sentiment']
     db.save(doc)
 
 
---
+++ SentimentGenerator/classifierGenerator3.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+import nltk 
+import csv 
+import itertools
+import re
+import pickle
+
+################################################################################
+def extract_features(document):
+    document_words = set(document)
+    features = {}
+    for word in word_features:
+        features[word] = (word in document_words)
+    return features
+################################################################################
+
+
+pattern = re.compile('(([A-z]{2,}(\'t)?))|([:;]o?\){1,3})|(>?[:;]o?\()')
+
+dirOfTraininData = './Training'
+dirOfWord_feature = './word_features.txt'
+dirOfClassifier = './classifier_Utral2.pickle'
+
+
+# The training tweets split
+TrainingTweets = []
+count = 0
+with open(dirOfTraininData) as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        if((count >= 150000 and count < 300000) or (count >= 950000 and count < 1100000)):
+            words = row[1]
+            sentiment = row[0]
+            words_filtered = pattern.findall(words)
+            new_words_filtered = []
+            for atuple in words_filtered:
+                atuple = filter(None, atuple)
+                new_words_filtered.append(atuple[0])
+                new_words_filtered = [x.lower() for x in new_words_filtered]
+            words_filtered =  new_words_filtered
+            TrainingTweets.append((words_filtered,sentiment))
+        count = count + 1
+            
+thefile = open(dirOfWord_feature, 'r')
+word_features = [line.strip() for line in thefile]
+thefile.close()
+
+# The real training data set
+training_set = nltk.classify.apply_features(extract_features, TrainingTweets)
+#print training_set
+
+# Use Naive Bayes as our traning model 
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+
+
+# Save the classifier
+f = open(dirOfClassifier, 'wb')
+pickle.dump(classifier, f)
+f.close()
+
+
+
+
+
