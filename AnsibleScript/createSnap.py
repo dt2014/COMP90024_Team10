faa8f826db37a8bdcd1cd68955c302499a7e68f0
chenvega <260435413@qq.com>
2015-05-19 13:58:59 +1000

    Update createSnap.py

--- AnsibleScript/createSnap.py
+++ AnsibleScript/createSnap.py
@@ -1,11 +1,11 @@
 '''
-* Team 10
-* New York
-* Fengmin Deng      659332
-* Jiajie Li         631482
-* Shuangchao Yin    612511
-* Weijia Chen       616213
-* Yun Shi           621761
+    * Team 10
+    * New York
+    * Fengmin Deng      659332
+    * Jiajie Li         631482
+    * Shuangchao Yin    612511
+    * Weijia Chen       616213
+    * Yun Shi           621761
 '''
 import boto
 from boto.ec2.regioninfo import RegionInfo
